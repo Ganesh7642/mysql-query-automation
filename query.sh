00125b4c4eeae25c53ed0d215d2c6fa36f52b8f2
ganesh7642 <ganeshkannan.ravishankar@gmail.com>
2025-03-16 16:38:16 +0000

    Updated_3

--- query.sh
+++ query.sh
@@ -25,7 +25,7 @@ QUERY="
 SELECT * FROM dept_emp limit 10;
 SELECT * FROM dept_emp_latest_date limit 15;
 SELECT * FROM dept_manager limit 15;
-SELECT * FROM employees limit 10;
+SELECT * FROM employees limit 15;
 SELECT * FROM salaries limit 10;
 SELECT * FROM titles limit 10;
 "
