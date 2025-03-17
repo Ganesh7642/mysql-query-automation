1f84675c6ebfbc40b57a26e175c5b89289ee1c68
ganesh7642 <ganeshkannan.ravishankar@gmail.com>
2025-03-17 07:47:34 +0000

    Updated_secret

--- query.sh
+++ query.sh
@@ -22,7 +22,7 @@ DB_PASS=$(echo "$SECRET_JSON" | jq -r '.password')
 
 # MySQL query
 QUERY="
-SELECT * FROM dept_emp limit 5;
+SELECT * FROM dept_emp limit 7;
 SELECT * FROM dept_emp_latest_date limit 5;
 SELECT * FROM dept_manager limit 5;
 SELECT * FROM employees limit 5;
